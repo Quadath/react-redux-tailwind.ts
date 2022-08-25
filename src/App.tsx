3079ad97afb98926013aae66e4eda039f99bde2d
Quadatz <quadath@gmail.com>
2022-08-25 11:27:09 +0300

    configure store

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { FavouritesPage } from './pages/FavouritesPage';
+import { HomePage } from './pages/HomePage';
+import { Navigation } from './components/navigation';
 
 function App() {
   return (
-    <h1 className='font-black'>Hello React!</h1>
+    <>
+      <Navigation/>
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/favourites" element={<FavouritesPage/>}/>
+      </Routes>
+    </>
   );
 }
 
