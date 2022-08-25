4a9baeaad6b62a575d7acdff69ff64b4c1011f18
Quadatz <quadath@gmail.com>
2022-08-25 14:24:51 +0300

    RTK query github request

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { useSearchUsersQuery } from "../store/github/github.api";
 
 export function HomePage() {
+    const {isLoading, isError, data} = useSearchUsersQuery('Qunthunnan');
+    console.log(data)
     return (
         <div>home</div>
     )
